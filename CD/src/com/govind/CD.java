69edf6e1428e95fee38f3bfa4e04a9575f8b1211
govindgoyal00000 <54981534+govindgoyal00000@users.noreply.github.com>
2021-11-28 20:03:02 +0530

    Add files via upload

---
+++ CD/src/com/govind/CD.java
@@ -0,0 +1,17 @@
+package com.govind;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CD {
+
+	public static void main(String[] args) {
+		
+		Map<String,String>num=new TreeMap<>();
+		num.put("Ed Sheeran","Thinking Out Loud");	
+		num.put("Wiz Khalifa","Tee You Again");
+		num.put("Katy Perry","Roar");
+		System.out.println(num);
+	}
+
+}
